5f97eb0d43385855c4cd17b9a1d950012118140c
Alba Perez Cesar <ALBA.PEREZCESAR@PLEXUS.ES>
2021-05-19 15:24:48 +0200

    terminado

---
+++ TiendaInterfaz2/Metodos.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaInterfaz2
+{
+    class Metodos
+    {
+
+        public static void insertMarca(int codigo, string descripcion)
+        {
+            using (var context = new TIENDADBEntities())
+            {
+                var marca = new MARCA()
+                {
+                    Codigo = codigo,
+                    Descripcion = descripcion
+                };
+                context.MARCAs.Add(marca);
+
+                context.SaveChanges();
+            }
+        }
+
+        public static void insertTipoProducto(int codigo, string nombre)
+        {
+            using (var context = new TIENDADBEntities())
+            {
+                var tipoProducto = new TIPOPRODUCTO()
+                {
+                    Codigo = codigo,
+                    Nombre = nombre
+                };
+                context.TIPOPRODUCTOes.Add(tipoProducto);
+
+                context.SaveChanges();
+            }
+        }
+
+        public static void insertProducto(int marcaId, int tipoProductoId, string descripcion, string talle, string color, string precio, int stock)
+        {
+            using (var context = new TIENDADBEntities())
+            {
+                var producto = new PRODUCTO()
+                {
+                    MarcaId = marcaId,
+                    TipoProductoId = tipoProductoId,
+                    Descripcion = descripcion,
+                    Talle = talle,
+                    Color = color,
+                    Precio = Decimal.Parse(precio),
+                    Stock = stock
+
+                };
+                context.PRODUCTOes.Add(producto);
+
+                context.SaveChanges();
+            }
+        }
+
+        public static string selectProducts(List<PRODUCTO> query)
+        {
+            string text = "";
+            using (var context = new TIENDADBEntities())
+            {
+
+                foreach (var product in query)
+                {
+
+                    text += "ProductoId: " + product.ProductoId + ", MarcaId: " + product.MarcaId + ", TipoProductoId: " + product.TipoProductoId +
+                        ", Descripcion: " + product.Descripcion + ", Talle: " + product.Talle + ", Color: " + product.Color + ", Precio: " + product.Precio + ", Stock: " + product.Stock + "\n";
+                }
+
+            }
+            return text;
+        }
+
+        public static List<PRODUCTO> getQuery()
+        {
+            Console.WriteLine("Buscar producto por:\n1. ProductoId\n2. MarcaId\n3. TipoProductoId\n4. Talle\n5. Color\n6. Precio\n7. Stock");
+            int opcion = Int32.Parse(Console.ReadLine());
+            int codigo;
+            string descripcion = "";
+            decimal precio;
+            var context = new TIENDADBEntities();
+            var query = context.PRODUCTOes.ToList();
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Introducir productoId: ");
+                    codigo = Int32.Parse(Console.ReadLine());
+                    query = context.PRODUCTOes.Where(s => s.ProductoId == codigo).ToList();
+                    break;
+                case 2:
+                    Console.WriteLine("Introducir MarcaId: ");
+                    codigo = Int32.Parse(Console.ReadLine());
+                    query = context.PRODUCTOes.Where(s => s.MarcaId == codigo).ToList();
+                    break;
+                case 3:
+                    Console.WriteLine("Introducir TipoProductoId: ");
+                    codigo = Int32.Parse(Console.ReadLine());
+                    query = context.PRODUCTOes.Where(s => s.TipoProductoId == codigo).ToList();
+                    break;
+                case 4:
+                    Console.WriteLine("Introducir Talle: ");
+                    descripcion = Console.ReadLine();
+                    query = context.PRODUCTOes.Where(s => s.Talle == descripcion).ToList();
+                    break;
+                case 5:
+                    Console.WriteLine("Introducir color: ");
+                    descripcion = Console.ReadLine();
+                    query = context.PRODUCTOes.Where(s => s.Color == descripcion).ToList();
+                    break;
+                case 6:
+                    Console.WriteLine("Introducir precio: ");
+                    precio = Decimal.Parse(Console.ReadLine());
+                    query = context.PRODUCTOes.Where(s => s.Precio == precio).ToList();
+                    break;
+                case 7:
+                    Console.WriteLine("Introducir stock: ");
+                    codigo = Int32.Parse(Console.ReadLine());
+                    query = context.PRODUCTOes.Where(s => s.Stock == codigo).ToList();
+                    break;
+                default:
+                    Console.WriteLine("Input no valido");
+                    break;
+            }
+            return query;
+        }
+    }
+}
---
+++ TiendaInterfaz2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TiendaInterfaz2
+{
+    class Program
+    {
+        /*
+         * -Crear un proyecto y utilizando entity framework permitir ingresar datos en las tablas MARCA, TIPOPRODUCTO y PRODUCTO
+         * -En otra pantalla, listar todos los productos, agegar un control para poder buscar y filtrar en la lista, permitir seleccionar productos y generar un TICKET con los productos seleccionados.
+         */
+
+        static void Main(string[] args)
+        {
+
+            int codigo;
+            string descripcion = "";
+            var context = new TIENDADBEntities();
+            var query = context.PRODUCTOes.ToList();
+
+            while (true)
+            {
+                Console.WriteLine("Elige una opción: \n1. Insertar datos en la tabla MARCA\n2. Insertar datos en la tabla TIPOPRODUCTO\n3. Insertar datos en la tabla PRODUCTO\n" +
+            "4. Listar todos los productos\n5. Buscar en la lista de productos\n6. Generar ticket con los productos seleccionados\nPulsa cualquier otro botón para salir");
+
+                int choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Añadir codigo: ");
+                        codigo = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Añadir descripcion: ");
+                        descripcion = Console.ReadLine();
+                        Metodos.insertMarca(codigo, descripcion);
+                        break;
+                    case 2:
+                        Console.WriteLine("Añadir codigo: ");
+                        codigo = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Añadir nombre: ");
+                        descripcion = Console.ReadLine();
+                        Metodos.insertTipoProducto(codigo, descripcion);
+                        break;
+                    case 3:
+                        Console.WriteLine("Añadir marcaId: ");
+                        codigo = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Añadir tipoProductoId: ");
+                        int id = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Añadir descripcion: ");
+                        descripcion = Console.ReadLine();
+                        Console.WriteLine("Añadir talle: ");
+                        string talle = Console.ReadLine();
+                        Console.WriteLine("Añadir color: ");
+                        string color = Console.ReadLine();
+                        Console.WriteLine("Añadir precio: ");
+                        string precio = Console.ReadLine();
+                        Console.WriteLine("Añadir stock: ");
+                        int stock = Int32.Parse(Console.ReadLine());
+                        Metodos.insertProducto(codigo, id, descripcion, talle, color, precio, stock);
+                        break;
+                    case 4:
+                        Console.WriteLine(Metodos.selectProducts(query));
+                        break;
+                    case 5:
+                        query = Metodos.getQuery();
+                        Console.WriteLine(Metodos.selectProducts(query));
+                        break;
+                    case 6:
+                        File.WriteAllText("prueba.txt", Metodos.selectProducts(query));
+                        break;
+                    default:
+                        Console.WriteLine("Saliendo...");
+                        break;
+
+                }
+
+                if (choice.Equals(0))
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
+            }
+
+        }
+    }
+
+
+
